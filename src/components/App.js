e07b87ffc35cf87352588b38e8bd2e2607c5bcf6
VENKATA_REDDY <venkatareddypadala1637@gmail.com>
2025-12-14 11:49:31 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ import Step from "./Step";
 const App = () => {
   const [step, setStep] = useState(1);
   return (
-    <div id={`step${step}`}>
+    <div>
       {/* Do not remove the main div */}
-      {/* <form onSubmit={(e) => e.preventDefault()}> */}
-      <Step setStep={setStep} step={step} />
-      {/* </form> */}
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Step setStep={setStep} step={step} />
+      </form>
     </div>
   );
 };
